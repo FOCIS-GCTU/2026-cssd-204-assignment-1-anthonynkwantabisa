a5f2891278931eb652644c4a3e6973611318f6f0
anthonynkwantabisa <2425401055@live.gctu.edu.gh>
2026-05-31 14:32:20 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description:Print out my initials in large block letters
+# Assignment Number: 1
+#
+# Name: Anthony Boateng Nkwantabisa
+# STUDENT ID: 2425401055
+# Email: 2425401055@live.gctu.edu.gh
+# Grader:
+#
+# On my honor, Anthony Boateng Nkwantabisa, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
-
+    # Main function to print out my initials in standard and large format.
+    print()
+    print("...ABN")
+    print()
+    print("......AAAA.........BBBBBBBBBB......NN........NN............")
+    print(".....AA..AA........BB........BB....NNN.......NN............")
+    print("....AA....AA.......BB........BB....NN.N......NN............")
+    print("...AA......AA......BB........BB....NN..N.....NN............")
+    print("...AAAAAAAAAA......BBBBBBBBBB......NN...N....NN............")
+    print("...AA......AA......BB........BB....NN....N...NN............")
+    print("...AA......AA......BB........BB....NN.....N..NN............")
+    print("...AA......AA......BB........BB....NN......NNNN............")
+    print("...AA......AA......BBBBBBBBBB......NN.......NNN.**.**")
+    print("................................................**.**")
+    print()
 
 
 main()
+
+
+
